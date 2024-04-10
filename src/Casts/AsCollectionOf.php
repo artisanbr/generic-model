88d87dfcdf0104d1cdbcd59641d44db68e22244c
renalcio <renalcio.c@gmail.com>
2024-04-10 13:25:17 -0300

    fix("set" cast method): Namespaces
    Fix return with key
    
    [skip ci]

--- src/Casts/AsCollectionOf.php
+++ src/Casts/AsCollectionOf.php
@@ -42,7 +42,8 @@ class AsCollectionOf extends AsCollection
 
                 $json_value = is_string($value) ? $value : GenericCollection::wrap($value ?? [])->toJson();
 
-                return [$key => $json_value];
+                //return [$key => $json_value];
+                return $json_value;
             }
         };
     }
